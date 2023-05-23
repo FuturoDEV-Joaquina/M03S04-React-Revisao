68c92d96f4d87fcbff01099b3ef52cd2f649bc6e
Rosana Rezende <rezende_rosana@hotmail.com>
2023-05-23 20:04:51 -0300

    adiciona rotas e páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Contact from './pages/Contact/Contact'
+import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
+
 function App() {
 
   return (
-    <>
-     Olá Mundo!
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Rota: / */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Navigate replace to="/" />} />
+
+        {/* Rota: /contato */}
+        <Route path="/contato" element={<Contact />} />
+
+        {/* Qualquer outra rota */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
